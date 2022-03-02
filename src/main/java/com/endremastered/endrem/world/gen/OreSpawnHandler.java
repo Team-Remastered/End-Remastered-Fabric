a96c883905f44cb934d534dd0fe005e5d4e18336
Jack-Bagel <ilysalhi22@gmail.com>
2022-03-01 20:17:54 -0500

    Updated Ore Generation

--- src/main/java/com/endremastered/endrem/world/gen/OreSpawnHandler.java
+++ src/main/java/com/endremastered/endrem/world/gen/OreSpawnHandler.java
@@ -1,5 +1,6 @@
 package com.endremastered.endrem.world.gen;
 
+import com.endremastered.endrem.EndRemastered;
 import com.endremastered.endrem.registry.ERBlocks;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
@@ -11,10 +12,11 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.decorator.*;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 
 
@@ -23,41 +25,45 @@ public class OreSpawnHandler {
    private static ConfiguredFeature<?, ?> END_CRYSTAL_ORE_GEN_BLACKSTONE = Feature.ORE
            .configure(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.BLACKSTONE),
                    ERBlocks.END_CRYSTAL_ORE.getDefaultState(),
-                   3, 0.5F))
-           .range(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.aboveBottom(0), YOffset.fixed(150)))) // Inclusive min and max height
-           .spreadHorizontally()
-           .repeat(20); // Number of veins per chunk
+                   3, 0));
+
+   public static PlacedFeature END_CRYSTAL_ORE_GEN_BLACKSTONE_PLACED_FEATURE = END_CRYSTAL_ORE_GEN_BLACKSTONE.withPlacement(
+           CountPlacementModifier.of(20),
+           SquarePlacementModifier.of(),
+           HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(100)));
 
     private static ConfiguredFeature<?, ?> END_CRYSTAL_ORE_GEN_POLISHED_BLACKSTONE_BRICKS = Feature.ORE
             .configure(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.POLISHED_BLACKSTONE_BRICKS),
                     ERBlocks.END_CRYSTAL_ORE.getDefaultState(),
-                    3, 0.5F))
-            .range(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.aboveBottom(0), YOffset.fixed(150)))) // Inclusive min and max height
-            .spreadHorizontally()
-            .repeat(20); // Number of veins per chunk
+                    3, 0));
+
+    public static PlacedFeature END_CRYSTAL_ORE_GEN_POLISHED_BLACKSTONE_BRICKS_PLACED_FEATURE = END_CRYSTAL_ORE_GEN_BLACKSTONE.withPlacement(
+            CountPlacementModifier.of(20),
+            SquarePlacementModifier.of(),
+            HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(100)));
 
     private static ConfiguredFeature<?, ?> END_CRYSTAL_ORE_GEN_CRACKED_POLISHED_BLACKSTONE_BRICKS = Feature.ORE
             .configure(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS),
                     ERBlocks.END_CRYSTAL_ORE.getDefaultState(),
-                    3, 0.5F))
-            .range(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.aboveBottom(0), YOffset.fixed(150)))) // Inclusive min and max height
-            .spreadHorizontally()
-            .repeat(20); // Number of veins per chunk
+                    3, 0));
+
+    public static PlacedFeature END_CRYSTAL_ORE_GEN_CRACKED_POLISHED_BLACKSTONE_BRICKS_PLACED_FEATURE = END_CRYSTAL_ORE_GEN_BLACKSTONE.withPlacement(
+            CountPlacementModifier.of(20),
+            SquarePlacementModifier.of(),
+            HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(100)));
 
    public static void init() {
-       RegistryKey<ConfiguredFeature<?, ?>> end_crystal_ore_gen_b = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
-               new Identifier("endrem", "end_crystal_ore_gen_b"));
-       Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, end_crystal_ore_gen_b.getValue(), END_CRYSTAL_ORE_GEN_BLACKSTONE);
-       BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, end_crystal_ore_gen_b);
 
-       RegistryKey<ConfiguredFeature<?, ?>> end_crystal_ore_gen_pbb = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
-               new Identifier("endrem", "end_crystal_ore_gen_pbb"));
-       Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, end_crystal_ore_gen_pbb.getValue(), END_CRYSTAL_ORE_GEN_POLISHED_BLACKSTONE_BRICKS);
-       BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, end_crystal_ore_gen_pbb);
+       Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, EndRemastered.createIdentifier("end_crystal_ore_gen_b"), END_CRYSTAL_ORE_GEN_BLACKSTONE);
+       Registry.register(BuiltinRegistries.PLACED_FEATURE, EndRemastered.createIdentifier("end_crystal_ore_gen_b"), END_CRYSTAL_ORE_GEN_BLACKSTONE_PLACED_FEATURE);
+       BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, EndRemastered.createIdentifier("end_crystal_ore_gen_b")));
 
-       RegistryKey<ConfiguredFeature<?, ?>> end_crystal_ore_gen_cpbb = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
-               new Identifier("endrem", "end_crystal_ore_gen_cpbb"));
-       Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, end_crystal_ore_gen_cpbb.getValue(), END_CRYSTAL_ORE_GEN_CRACKED_POLISHED_BLACKSTONE_BRICKS);
-       BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, end_crystal_ore_gen_cpbb);
+       Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, EndRemastered.createIdentifier("end_crystal_ore_gen_pbb"), END_CRYSTAL_ORE_GEN_POLISHED_BLACKSTONE_BRICKS);
+       Registry.register(BuiltinRegistries.PLACED_FEATURE, EndRemastered.createIdentifier("end_crystal_ore_gen_pbb"), END_CRYSTAL_ORE_GEN_POLISHED_BLACKSTONE_BRICKS_PLACED_FEATURE);
+       BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, EndRemastered.createIdentifier("end_crystal_ore_gen_pbb")));
+
+       Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, EndRemastered.createIdentifier("end_crystal_ore_gen_cpbb"), END_CRYSTAL_ORE_GEN_CRACKED_POLISHED_BLACKSTONE_BRICKS);
+       Registry.register(BuiltinRegistries.PLACED_FEATURE, EndRemastered.createIdentifier("end_crystal_ore_gen_cpbb"), END_CRYSTAL_ORE_GEN_CRACKED_POLISHED_BLACKSTONE_BRICKS_PLACED_FEATURE);
+       BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, EndRemastered.createIdentifier("end_crystal_ore_gen_cpbb")));
    }
 }

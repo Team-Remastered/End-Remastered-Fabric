3a205481100bcda98aa6e41fb640301b79816842
Jack-Bagel <ilysalhi22@gmail.com>
2022-10-14 14:52:55 -0400

    Added customizable Loot Injection

--- src/main/java/com/teamremastered/endrem/util/LootInjection.java
+++ src/main/java/com/teamremastered/endrem/util/LootInjection.java
@@ -1,5 +1,6 @@
 package com.teamremastered.endrem.util;
 
+import com.teamremastered.endrem.config.ERConfigHandler;
 import com.teamremastered.endrem.registry.ERItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
@@ -8,28 +9,51 @@ import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.util.Identifier;
 
+
 public class LootInjection {
 
+    //Function to get the injection path and ModId
+
+    private static String getInjection(String lootTableID, Boolean isModId) {
+
+        String[] injectionParts = lootTableID.split(":");
+        String modId = injectionParts[0];
+        String path = injectionParts[1];
+
+        if (isModId) {
+            return modId;
+        }
+        else {
+            return  path;
+        }
+    }
+
     /* Chests */
-    private static final Identifier MINESHAFT_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/abandoned_mineshaft");
-    private static final Identifier BURIED_TREASURE_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/buried_treasure");
-    private static final Identifier DESERT_PYRAMID_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/desert_pyramid");
-    private static final Identifier IGLOO_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/igloo_chest");
-    private static final Identifier JUNGLE_TEMPLE_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/jungle_temple");
-    private static final Identifier NETHER_BRIDGE_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/nether_bridge");
-    private static final Identifier OUTPOST_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/pillager_outpost");
-    private static final Identifier SHIPWRECK_TREASURE_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/shipwreck_treasure");
-    private static final Identifier DUNGEON_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/simple_dungeon");
-    private static final Identifier BASTION_TREASURE_LOOT_TABLE_ID = new Identifier("minecraft", "chests/bastion_treasure");
-    private static final Identifier WOODLAND_MANSION_LOOT_TABLE_ID = new Identifier("minecraft", "chests/woodland_mansion");
+    private static final Identifier JUNGLE_TEMPLE_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.ROGUE_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.ROGUE_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier OUTPOST_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.CORRUPTED_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.CORRUPTED_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier BURIED_TREASURE_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.BLACK_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.BLACK_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier MINESHAFT_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.LOST_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.LOST_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier DESERT_PYRAMID_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.OLD_EYE_LOOT_TABLE_ID, true), getInjection(ERConfigHandler.OLD_EYE_LOOT_TABLE_ID, false));
+
+    private static final Identifier IGLOO_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.COLD_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.COLD_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier NETHER_BRIDGE_CHEST_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.NETHER_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.NETHER_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier BASTION_TREASURE_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.CURSED_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.CURSED_EYE_LOOT_TABLE_ID,false));
+
+    private static final Identifier WOODLAND_MANSION_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.MAGICAL_EYE_MANSION_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.MAGICAL_EYE_MANSION_LOOT_TABLE_ID,false));
 
 
     /* Entities */
-    private static final Identifier EVOKER_LOOT_TABLE_ID = new Identifier("minecraft", "entities/evoker");
-    private static final Identifier WITHER_LOOT_TABLE_ID = new Identifier("minecraft", "entities/wither");
-    private static final Identifier ELDER_GUARDIAN_LOOT_TABLE_ID = new Identifier("minecraft", "entities/elder_guardian");
-    private static final Identifier WITCH_LOOT_TABLE_ID = new Identifier("minecraft", "entities/witch");
-    private static final Identifier SKELETON_HORSE_LOOT_TABLE_ID = new Identifier("minecraft", "entities/skeleton_horse");
+    private static final Identifier EVOKER_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.MAGICAL_EYE_EVOKER_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.MAGICAL_EYE_EVOKER_LOOT_TABLE_ID,false));
+    private static final Identifier WITHER_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.WITHER_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.WITHER_EYE_LOOT_TABLE_ID,false));
+    private static final Identifier ELDER_GUARDIAN_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.GUARDIAN_EYE_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.GUARDIAN_EYE_LOOT_TABLE_ID,false));
+    private static final Identifier WITCH_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.WITCH_PUPIL_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.WITCH_PUPIL_LOOT_TABLE_ID,false));
+    private static final Identifier SKELETON_HORSE_LOOT_TABLE_ID = new Identifier(getInjection(ERConfigHandler.UNDEAD_SOUL_LOOT_TABLE_ID,true), getInjection(ERConfigHandler.UNDEAD_SOUL_LOOT_TABLE_ID,false));
 
 
 
@@ -39,66 +63,64 @@ public class LootInjection {
             if (JUNGLE_TEMPLE_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F)) // Same as "rolls": 1 in the loot table json
-                        .conditionally(RandomChanceLootCondition.builder(0.4F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.ROGUE_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.ROGUE_EYE));
                 table.pool(poolBuilder);
             } else if (OUTPOST_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.3F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.CORRUPTED_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.CORRUPTED_EYE));
                 table.pool(poolBuilder);
             }
             else if (BURIED_TREASURE_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.3F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.BLACK_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.BLACK_EYE));
                 table.pool(poolBuilder);
             }
             else if (MINESHAFT_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.2F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.LOST_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.LOST_EYE));
-                table.pool(poolBuilder);
-            }
-            else if (DUNGEON_CHEST_LOOT_TABLE_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.05F))
-                        .with(ItemEntry.builder(ERItems.LOST_EYE))
-                        .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.05F))
-                        .with(ItemEntry.builder(ERItems.OLD_EYE));
                 table.pool(poolBuilder);
             }
             else if (DESERT_PYRAMID_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.10F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.OLD_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.OLD_EYE));
                 table.pool(poolBuilder);
             }
             else if (IGLOO_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.70F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.COLD_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.COLD_EYE));
                 table.pool(poolBuilder);
             }
             else if (NETHER_BRIDGE_CHEST_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.3F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.NETHER_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.NETHER_EYE));
                 table.pool(poolBuilder);
             }
-            else if (SHIPWRECK_TREASURE_CHEST_LOOT_TABLE_ID.equals(id)) {
+
+            else if (BASTION_TREASURE_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.05F))
-                        .with(ItemEntry.builder(ERItems.BLACK_EYE));
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.CURSED_EYE_WEIGHT))
+                        .with(ItemEntry.builder(ERItems.CURSED_EYE));
+                table.pool(poolBuilder);
+            }
+            else if (WOODLAND_MANSION_LOOT_TABLE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1.0F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.MAGICAL_EYE_MANSION_WEIGHT))
+                        .with(ItemEntry.builder(ERItems.MAGICAL_EYE));
                 table.pool(poolBuilder);
             }
 
@@ -106,49 +128,35 @@ public class LootInjection {
             else if (EVOKER_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.05F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.MAGICAL_EYE_EVOKER_WEIGHT))
                         .with(ItemEntry.builder(ERItems.MAGICAL_EYE));
                 table.pool(poolBuilder);
             }
             else if (WITHER_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(2.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.9F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.WITHER_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.WITHER_EYE));
                 table.pool(poolBuilder);
             }
             else if (ELDER_GUARDIAN_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.3F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.GUARDIAN_EYE_WEIGHT))
                         .with(ItemEntry.builder(ERItems.GUARDIAN_EYE));
                 table.pool(poolBuilder);
             }
             else if (WITCH_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.10F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.WITCH_PUPIL_WEIGHT))
                         .with(ItemEntry.builder(ERItems.WITCH_PUPIL));
-                table.pool(poolBuilder);
-            }
-            else if (BASTION_TREASURE_LOOT_TABLE_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.5F))
-                        .with(ItemEntry.builder(ERItems.CURSED_EYE));
-                table.pool(poolBuilder);
-            }
-            else if (WOODLAND_MANSION_LOOT_TABLE_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.1F))
-                        .with(ItemEntry.builder(ERItems.MAGICAL_EYE));
                 table.pool(poolBuilder);
             }
             else if (SKELETON_HORSE_LOOT_TABLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0F))
-                        .conditionally(RandomChanceLootCondition.builder(0.25F))
+                        .conditionally(RandomChanceLootCondition.builder(ERConfigHandler.UNDEAD_SOUL_WEIGHT))
                         .with(ItemEntry.builder(ERItems.UNDEAD_SOUL));
                 table.pool(poolBuilder);
             }

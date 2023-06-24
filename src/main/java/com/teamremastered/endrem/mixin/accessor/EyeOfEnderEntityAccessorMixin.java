15346de645c4a6e662b9356ef22f0c1b84931935
Jack <ilysalhi22@gmail.com>
2023-06-24 10:44:19 -0400

    Changed Frame Texture to Update with Texture Packs

--- src/main/java/com/teamremastered/endrem/mixin/accessor/EyeOfEnderEntityAccessorMixin.java
+++ src/main/java/com/teamremastered/endrem/mixin/accessor/EyeOfEnderEntityAccessorMixin.java
@@ -1,6 +1,6 @@
 package com.teamremastered.endrem.mixin.accessor;
 
-import net.minecraft.entity.EyeOfEnderEntity;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
